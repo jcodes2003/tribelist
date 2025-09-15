b6843c4ea1862d02b5c8204b0959f0dc6e017175
jcodes2003 <calmaj2003@gmail.com>
2025-09-15 13:54:36 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,320 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from "react";
 
-export default function Home() {
+function App() {
+  type Tab = "Freshmen" | "Sophomores" | "Juniors" | "Seniors";
+
+  const [activeTab, setActiveTab] = useState<Tab>("Freshmen");
+  const [query, setQuery] = useState<string>("");
+
+  const freshmen = [
+    "ABALDE, JELLIAN MAHUNYAG",
+    "ACENAS, JOSHUA JAMES PACANA",
+    "AMBA, JON DENVER SALOMON",
+    "AMPONG, ADRIAN MELAUR",
+    "ATLAO, KYLE YBAÑEZ",
+    "BAHIAN, JASPHER BALASABAS",
+    "BALBON, SAMMUEL JAMES VERGARA",
+    "BOLANIO, MARIA ELIZABETH MUSA",
+    "BUCIO, KIRLYN JADE LABO",
+    "CALUSTRE, JUNE MICHAEL POSADAS",
+    "CAMILOTES, ELDRIDGE BERNARD CAPILI",
+    "CASIDAR, JOHARY PANGO",
+    "DESOYO, CHRISTIAN JAMES PELIGRINO",
+    "DIGAL, JETHRO ADAM CARANYAGAN",
+    "DULANAS, FERD DENVER ORMICHIGOS",
+    "EDORIA, JONMAR SACAY",
+    "ESPINOLA, BRYAN CHRISTOPHER VALLECERA",
+    "ESTOMATA, CRIS RAPHAEL PELIGRO",
+    "FELISAN, STEPHANIE MAE AQUIATAN",
+    "HERRERA, CYNTHIA DE GUZMAN",
+    "IGOT, ADRIAN FERNANDEZ",
+    "JACALAN, FRANCIS MANGADLAO",
+    "JUMAWAN, ROY DELA RAMA",
+    "JUSAY, JOHN ELLY GUMAGABAN",
+    "LABUNTOG, EZEKEIL BALILI",
+    "LADLAD, SHUN MIKEL BACLAYO",
+    "LADRA, STEPHANIE FAITH TALAO",
+    "LAUS, JUSTEN CAPURAS",
+    "LOBITAÑA, JHON MICCO",
+    "MABITAG, RONALD PADERAN",
+    "MACAPAYAG, KISHA TOMTOM",
+    "MOLO, KIP KIENO ECONAR",
+    "ORQUILLAS, CHRISTINE ANGEL DURERO",
+    "PADER, ANGELO MORILLA",
+    "PARAGOSO, JHAMER BUMATAY",
+    "PAT, MICOH JHON LANUTAN",
+    "PLACA, JADE LOPEZ",
+    "PULLOS, CHIQUEZYLAH",
+    "QUEJOY, JEFFER JAN SONQUIPAL",
+    "REBUYON, ERVEN DENOSTA",
+    "RIVERA, HONEY GRACE BUENO",
+    "ROBLE, CHRYSLER JOHN MIÑOZA",
+    "SERATO, ANTONETTE HIAN",
+    "SIMBAJON, SAM DENVER PIZON",
+    "SULTAN, JEVY GUMBAN",
+    "SUMAYLO, JASMINE SUMALINOG",
+    "TACUGUE, JILLIAN FAITH VILLANUEVA",
+    "TUMAPON, NEIL PATRICK LABASTIDA",
+    "UNDURAN, SYRA ECAT",
+    "VILLANUEVA, SILVER KIAN SALIGUMBA",
+    "VITERBO, XYRILE IVAN BUTAL",
+    "YU, BENEDICT HAMTIG",
+    "ZAFRA, JUVYLLE ABRAGAN",
+    "ABECIA, JOHN TOVEN PANGANIBAN",
+    "ALABA, CLENT DARYLLE LUMBAY",
+    "ALOMBRO, JOSEPH HANDEL BERMAS",
+    "APOR, VENICE BELLE LUNA",
+    "ARAGON, ANGEL",
+    "BAHADE, KURTCHEYNE MAY LAGO",
+    "BALATERO, ACE QUIJANO",
+    "BARRIENTOS, CHRISTIAN",
+    "BIDOY, JEBON ACE BALAMBAN",
+    "CABALIDA, JOHN ALFERD ORNEJAS",
+    "CABREROS, MICHAEL REYBEN ESTAFIA",
+    "CABUGATAN, HOWARD DWAINE SANDIGAN",
+    "CALLAO, ASHLEY JAMES PANJAO",
+    "CAPIN, ANGEL MAE PANGANORON",
+    "CASPE, JURIZTINE CABLINDA",
+    "CHICO, WENDELL CANSAY",
+    "COMING, MILDRED C",
+    "DABLIO, JAMES CEDRIC MUEGO",
+    "DACLAG, JOSUA BAGTASOS",
+    "DACUP, AGOTINE JAMES LLAMSO",
+    "DAITON, JUAN EMMANUEL PAGARAN",
+    "DIMANG, AINA HADJIDAUD",
+    "EMBALZADO, JULIUS CAESAR BUSTAMANTE",
+    "EPONDOLAN, JOHN ANDREI NICDAO",
+    "ESPINOSA, GABRYLLE QUINO",
+    "EXCLAMADOR, CARL VINCENT AGUILAR",
+    "GALARPE, JIA OTAW",
+    "GIANAN, CHRIS ADRIAN QUIMANHAN",
+    "GILIG, PRINCESS MAE SUGIAN",
+    "IBARRA, GEOFF MANUEL TACULOD",
+    "JAMPIT, BRIX HUNTER ZARATE",
+    "JARAULA, CALEB BAYO",
+    "JAUDIAN, JONATHAN CARIAS",
+    "KILATON, BHONG SABALSA",
+    "LAGRA, JOHN RICA GARCIA",
+    "MACABODBOD, RYAN DAVE TARDE",
+    "MARSAMOLO, SAMANTHA LENSAY PACUPIA",
+    "MEDINA, ANGELO JAY BARCE",
+    "MONTANO, DERICK MAGADA",
+    "OCTAVIO, KLENT MANGLICMOT",
+    "PALCO, VENICE SUGANOB",
+    "PATAYO, KENNETH CRAZO",
+    "PEREZ, DIAN SUMAIT",
+    "PERGIS, TRISHA JANE CAILING",
+    "RALAR, JONATHAN",
+    "RELAMPAGOS, TJ BRIAN AMPAHAN",
+    "ROSEL, KATE OBLINA",
+    "SACAY, KEVIN JAY JAMPIT",
+    "SALVAÑA, MIL JYPRUS ASENTISTA",
+    "SAMPORNA, NATHALIE FAITH ALIA",
+    "TAN, KLINT IVAN",
+    "UBANAN, AJ LLOYD AGNES",
+    "VELARDE, MARIELLE JOY RICALDE",
+  ];
+
+  const sophomores = [
+    "ANGGA, RINIL JAN CARBONERO",
+    "APOSTOL, CRISTIAN DAVE ABREZ",
+    "BABAYSON, ROSHIEL GRACE AURIT",
+    "BACASMOT, ISAACJAMES PIALAN",
+    "BARILLO, JULAM VIC ADLAON",
+    "BAS, DIANA MARIE SACOLINGGAN",
+    "BAYAON, LESLIE ANNE",
+    "BAYLOSIS, FROILAN REY BUGAY",
+    "BELAYO, CLARK TOLENTINO",
+    "BINASAG, JHONLLOYD ORIBE",
+    "CABIGQUEZ, XYREX GEM SUDARIO",
+    "CABILI, EDWARD B.",
+    "CAILO, ALEXANDRA FAITH MAQUE",
+    "CASAS, MARVEN REY MAGHANOY",
+    "DALAUTA, CARL VINCENT DIANGO",
+    "DELITO, JENNEATH CABANDAY",
+    "DIESTA, JULE ANDREW MAHISTRADO",
+    "DIGABE, KASSANDRA KATE EGAGAMAO",
+    "DIZON, EZREIL JAMES PORGARILLAS",
+    "ELISON, CEDRIC ELLARINA",
+    "ESLIT, ANGELINE BALUCAN",
+    "ESPERON, EMANUEL DAVE TORRALBA",
+    "ESTRADA, CHRISTIAN DAVE SALOM",
+    "FUENTES, JASON GURRO",
+    "GALAGAR, BRENT C.J AGCOPRA",
+    "IGNACIO, HEART NACAYTUNA",
+    "INDOC, JHON PAUL SULINAY",
+    "LEGASPI, NOELLY AMOR",
+    "LLOBIA, CHRISTIAN UMASDANG",
+    "LORONO, RHEALIZA JALAGAT",
+    "LUMAHANG, DOCTORA ALEXA MERRY SAGALA",
+    "LUMBAN, KATE CHRISTINE CUBAO",
+    "LUMINDA, PHILIP LIBOGAN",
+    "MAANDIG, MEDWIN SENDON",
+    "MACABALE, HANNAH JEAN GALONO",
+    "MADLOS, JOHN MARK OLARIO",
+    "MAROHOM, HANNAH MARIE ROBLE",
+    "OBLIOSCA, NILE CHRISTIAN TAN",
+    "OLAERA, ALTHEA SHAIRA MAE SEVILLA",
+    "PAIGNA, JUSIE MER AMOLO",
+    "PALMARES, JERRY LORIGAS",
+    "PARCIA, CHEMIE VILLA",
+    "PEREZ, JOHN MICHAEL LADERA",
+    "RIVERAL, BHEA MARIE ALTARIBA",
+    "RUBIO, PRINCESS ALTHIA IGBALIC",
+    "VALMORIA, CHRISTINE SALVALEON",
+    "VILLALOBOS, MARY ZCALCY CABASAG",
+    "VILLASTIQUE, MARY ROSE UBOD",
+    "WAMINAL, SHAN CAÑETE",
+    "YANGA, MECCAELLA THEFANEE SABUERO",
+    "ANSING, KHYLA JANE CANTILLER",
+    "ANSUNGAY, STEPHEN CHARLS ACLARACION",
+    "ANTIDO, KIAN CARL BORGONIA",
+    "BALINAS, BON JHOVE GICANA",
+    "BANNISTER, JOEFREY PAKIS",
+    "BARTE, VINCENT JOHN",
+    "BUKHAMMAS, SAEED SAAD KHALIL MANSEGUIAO",
+    "CAÑETE, JOSHUA ABUZO",
+  ];
+
+  const juniors = [
+    "PACANA, SHANDY SEDON",
+    "PACLAR, KENNETH QUIOYO",
+    "PADERAN, DON REY GILIG",
+    "PADLA III, EDWIN ALBASETE",
+    "PAGALING, SANDARA JANE MABALE",
+    "PAGKALINAWAN, CHRISTINE SHAYNE LAPLAP",
+    "PAJE, CHRISTINE MAE FLORES",
+    "PALMA, RAULLYN JOY DACLAN",
+    "PALOMERAS, ANDRE Y.",
+    "PANONG, SANDY JEAN AMPORINGUES",
+    "PAO, JEOGER KIM ODCHIGUE",
+    "PARAJES, JESSIE JAMES CASTINO",
+    "PASIA, JAYCHELLE CANOMON",
+    "PAUSAL, JANSHIEN CAHOY",
+    "PERINO, VAN KYZER RANAY",
+    "PIMENTEL, CHARLIE GIN ABSIN",
+    "PIONAN, CHERRY JANE ARCAYENA",
+    "PLAZA, CLINT DENZEL VILLANUEVA",
+    "PORTRIAS, JOSH MARVIN PADABOC",
+    "PORTUGAL, SHENNIELYN AGUILAR",
+    "QUINTO, VINCENT NIEL DACLAG",
+    "RAAGAS, KNIGHT HEZEKIAH BARCENA",
+    "RABANAL, EDCHEL B.",
+    "RAGASI, BRIAN D.",
+    "RAL, LEOJHAY GAVIOLA",
+    "RALUTO, ANDREW JONES PELIGRO",
+    "RAMOS, RORRY AMPUYAS",
+    "RAOTRAOT, ASHLEY VALCORZA",
+  ];
+
+  const seniors = [
+    "BALDOZA, RAZIEL JADE DIAPANA",
+    "BUTAYA, CHRISTIAN COLIPANO",
+    "MACUSE, C JAY LOGONG",
+    "NERONA, PAUL ANDREW LARA",
+    "IROC, NATHANIEL BUTANAS",
+    "VERSOZA, IVAN KY LACHICA",
+    "LACHECA, BEA YSABEL MACALUA",
+    "JABULAN, MAE NUÑEZ",
+    "MACARIO, MEL ANGELO SABIDO",
+    "IGNALIG, KITTIM TANHAGA",
+    "BALIBAGON, MARY SOPHIA RANA",
+    "SACAY, GRIZON RUSSELL BANLAS",
+  ];
+
+  const lists: Record<Tab, string[]> = {
+    Freshmen: freshmen,
+    Sophomores: sophomores,
+    Juniors: juniors,
+    Seniors: seniors,
+  };
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Student Directory</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      {/* Tabs */}
+      <div className="flex space-x-4 mb-6">
+        {(Object.keys(lists) as Tab[]).map((year: Tab) => (
+          <button
+            key={year}
+            onClick={() => setActiveTab(year)}
+            className={`px-4 py-2 rounded-lg ${
+              activeTab === year
+                ? "bg-blue-600 text-white"
+                : "bg-gray-200 text-black"
+            }`}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            {year}
+          </button>
+        ))}
+      </div>
+
+      {/* Search + List Display */}
+      <div>
+        <h2 className="text-xl font-semibold mb-2">{activeTab}</h2>
+        <div className="mb-3 flex items-center gap-2">
+          <input
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder={`Search ${activeTab}...`}
+            className="flex-1 px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <button
+            onClick={() => setQuery("")}
+            className="px-3 py-2 bg-gray-200 rounded-md text-sm"
+            aria-label="Clear search"
           >
-            Read our docs
-          </a>
+            Clear
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <ul className="list-decimal list-inside space-y-1">
+          {lists[activeTab]
+            .filter((name) => name.toLowerCase().includes(query.toLowerCase()))
+            .map((name: string, index: number) => {
+              if (!query) {
+                return (
+                  <li key={index} className="text-sm">
+                    {name}
+                  </li>
+                );
+              }
+
+              // simple highlight: split around the matched substring (case-insensitive)
+              const lcName = name.toLowerCase();
+              const lcQuery = query.toLowerCase();
+              const start = lcName.indexOf(lcQuery);
+
+              if (start === -1) {
+                return (
+                  <li key={index} className="text-sm">
+                    {name}
+                  </li>
+                );
+              }
+
+              const before = name.slice(0, start);
+              const match = name.slice(start, start + query.length);
+              const after = name.slice(start + query.length);
+
+              return (
+                <li key={index} className="text-sm">
+                  {before}
+                  <span className="bg-yellow-200 px-1">{match}</span>
+                  {after}
+                </li>
+              );
+            })}
+
+          {lists[activeTab].filter((name) => name.toLowerCase().includes(query.toLowerCase())).length === 0 && (
+            <li className="text-sm text-gray-500">No results found.</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
+
+export default App;
